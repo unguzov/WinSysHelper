0a76c64205935f82a6e0b179d1e26b33f9f0076e
Nikolay Unguzov <34856415+unguzov@users.noreply.github.com>
2023-01-29 18:59:46 +0200

    Get-BatteryInfo update

--- modules/WinSysHelper.UPS/public/Get-BatteryInfo.ps1
+++ modules/WinSysHelper.UPS/public/Get-BatteryInfo.ps1
@@ -28,50 +28,48 @@ function Get-BatteryInfo {
         $ComputerName = $env:computername
     )
 
-    # $hasBattery = @(Get-CimInstance -ClassName Win32_Battery -ComputerName $ComputerName).Count -gt 0
+    $batStatus = @{
+        1   = "Battery Power"
+        2   =  "AC Power"
+        3   = "Fully Charged"
+        4   =  "Low"
+        5   = "Critical"
+        6   = "Charging"
+        7   = "Charging and High"
+        8   = "Charging and Low"
+        9   = "Charging and Critical"
+        10  = "Undefined"
+        11  = "Partially Charged"
+    }
+
+    $batAvailability =@{
+        1   = "Other" 
+        2   =  "Unknown" 
+        3   = "Running on Full Power"
+        4   =  "Warning" 
+        5   = "In Test"
+        6   = "Not Applicable"
+        7   = "Power Off"
+        8   = "Off Line"
+        9   = "Off Duty"
+        10  =  "Degraded"
+        11  =  "Not Installed"
+        12  =  "Install Error"
+        13  = "Power Save - Unknown"
+        14  = "Power Save - Low Power Mode" 
+        15  = "Power Save - Standby"
+        16  = "Power Cycle"
+        17  = "Power Save - Warning"
+        18  = "Paused"
+        19  = "Not Ready"
+        20  = "Not Configured"
+        21  = "Quiesced"
+        999 = "Not using battery"
+    }
 
     $batteries = Get-WmiObject -Class Win32_Battery -ComputerName $ComputerName
 
     if ($batteries) {
-
-        $batStatus = @{
-            1   = "Battery Power"
-            2   =  "AC Power"
-            3   = "Fully Charged"
-            4   =  "Low"
-            5   = "Critical"
-            6   = "Charging"
-            7   = "Charging and High"
-            8   = "Charging and Low"
-            9   = "Charging and Critical"
-            10  = "Undefined"
-            11  = "Partially Charged"
-        }
-
-        $batAvailability =@{
-            1   = "Other" 
-            2   =  "Unknown" 
-            3   = "Running on Full Power"
-            4   =  "Warning" 
-            5   = "In Test"
-            6   = "Not Applicable"
-            7   = "Power Off"
-            8   = "Off Line"
-            9   = "Off Duty"
-            10  =  "Degraded"
-            11  =  "Not Installed"
-            12  =  "Install Error"
-            13  = "Power Save - Unknown"
-            14  = "Power Save - Low Power Mode" 
-            15  = "Power Save - Standby"
-            16  = "Power Cycle"
-            17  = "Power Save - Warning"
-            18  = "Paused"
-            19  = "Not Ready"
-            20  = "Not Configured"
-            21  = "Quiesced"
-            999 = "Not using battery"
-        }
 
         $batteryInfo = foreach ($batt in $batteries) {
     
