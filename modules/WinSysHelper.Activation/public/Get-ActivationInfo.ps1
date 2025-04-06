5d77daaebc23ef6aeeb05accc70ae123c90cdb74
Nikolay Unguzov <34856415+unguzov@users.noreply.github.com>
2025-04-06 15:02:23 +0300

    Added key reader

--- modules/WinSysHelper.Activation/public/Get-ActivationInfo.ps1
+++ modules/WinSysHelper.Activation/public/Get-ActivationInfo.ps1
@@ -41,6 +41,28 @@ function Get-ActivationInfo {
         3221549568 = 'Grace time expired'
     }
 
+    # Try to get Windows Key
+
+    $map="BCDFGHJKMPQRTVWXY2346789"
+
+    $value = (get-itemproperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").digitalproductid[0x34..0x42]
+    $ProductKey = ""
+
+    for ($i = 24; $i -ge 0; $i--) {
+    $r = 0
+    for ($j = 14; $j -ge 0; $j--) {
+        $r = ($r * 256) -bxor $value[$j]
+        $value[$j] = [math]::Floor([double]($r / 24))
+        $r = $r % 24
+    }
+
+    $ProductKey = $map[$r] + $ProductKey
+
+    if (($i % 5) -eq 0 -and $i -ne 0) {
+        $ProductKey = "-" + $ProductKey
+    }
+    }
+
     $ainfo = Get-CimInstance SoftwareLicensingProduct `
     -Filter "Name like 'Windows%'" `
     | Where-Object { $_.PartialProductKey } `
@@ -58,9 +80,12 @@ function Get-ActivationInfo {
         'LicenseStatusReason' = $ainfo.LicenseStatusReason
         'ProductKeyChannel' = $ainfo.ProductKeyChannel
         'PartialProductKey' = $ainfo.PartialProductKey
+        'GuessedProductKey' = $ProductKey
         'WindowsProductName' = $compInfo.WindowsProductName
         'WindowsVersion' = $compInfo.WindowsVersion
         'OsHardwareAbstractionLayer' = $compInfo.OsHardwareAbstractionLayer
         'OsInstallDate' = $compInfo.OsInstallDate
         })
+
+        
 }
