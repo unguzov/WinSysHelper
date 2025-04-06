48fef6b9bf2811290de0e8e61c017a77b69cb3dd
Nikolay Unguzov <34856415+unguzov@users.noreply.github.com>
2025-04-06 15:06:07 +0300

    Key read

--- modules/WinSysHelper.Activation/public/Get-ActivationInfo.ps1
+++ modules/WinSysHelper.Activation/public/Get-ActivationInfo.ps1
@@ -49,18 +49,18 @@ function Get-ActivationInfo {
     $ProductKey = ""
 
     for ($i = 24; $i -ge 0; $i--) {
-    $r = 0
-    for ($j = 14; $j -ge 0; $j--) {
-        $r = ($r * 256) -bxor $value[$j]
-        $value[$j] = [math]::Floor([double]($r / 24))
-        $r = $r % 24
-    }
+        $r = 0
+        for ($j = 14; $j -ge 0; $j--) {
+            $r = ($r * 256) -bxor $value[$j]
+            $value[$j] = [math]::Floor([double]($r / 24))
+            $r = $r % 24
+        }
 
-    $ProductKey = $map[$r] + $ProductKey
+        $ProductKey = $map[$r] + $ProductKey
 
-    if (($i % 5) -eq 0 -and $i -ne 0) {
-        $ProductKey = "-" + $ProductKey
-    }
+        if (($i % 5) -eq 0 -and $i -ne 0) {
+            $ProductKey = "-" + $ProductKey
+        }
     }
 
     $ainfo = Get-CimInstance SoftwareLicensingProduct `
